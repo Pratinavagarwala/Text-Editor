21093f6e3f9b37bcf29a88b47408bb22d61701cb
Pratinavagarwala <66483787+Pratinavagarwala@users.noreply.github.com>
2023-01-24 18:43:54 +0530

    Update home.js

--- home.js
+++ home.js
@@ -1,55 +1,63 @@
-function f1() {
+function bold() {
 	//function to make the text bold using DOM method
 	document.getElementById("textarea1").style.fontWeight = "bold";
 }
 
-function f2() {
+function fitalics() {
 	//function to make the text italic using DOM method
 	document.getElementById("textarea1").style.fontStyle = "italic";
 }
 
-function f3() {
+function flAlign() {
 	//function to make the text alignment left using DOM method
 	document.getElementById("textarea1").style.textAlign = "left";
 }
 
-function f4() {
+function cAlign() {
 	//function to make the text alignment center using DOM method
 	document.getElementById("textarea1").style.textAlign = "center";
 }
 
-function f5() {
+function rAlign() {
 	//function to make the text alignment right using DOM method
 	document.getElementById("textarea1").style.textAlign = "right";
 }
 
-function f6() {
+function uCase() {
 	//function to make the text in Uppercase using DOM method
 	document.getElementById("textarea1").style.textTransform = "uppercase";
 }
 
-function f7() {
- if(window.clipboardData) {   
-   document.getElementById('txToPaste').value = window.clipboardData.getData("Text");
- } 
+function tPaste() {
+//  if(window.clipboardData) {   
+//    document.getElementById('txToPaste').value = window.clipboardData.getData("Text");
+//  } 
+navigator.clipboard.readText().then(
+    clipText => document.querySelector(".input").value += clipText);
 }
 
-function f8() {
- if(window.clipboardData) {
-   window.clipboardData.clearData();
-   window.clipboardData.setData("Text", document.getElementById('txToCopy').value);
- } 
+function tCopy() {
+ // Get the text field
+ var copyText = document.getElementById("textarea1");
+
+ // Select the text field
+ copyText.select();
+ copyText.setSelectionRange(0, 99999); // For mobile devices
+
+  // Copy the text inside the text field
+ navigator.clipboard.writeText(copyText.value);
+
 }
 
 
 
-function f9() {
+function tClear() {
 	//function to make the text back to normal by removing all the methods applied
 	//using DOM method
-	document.getElementById("textarea1").style.fontWeight = "normal";
-	document.getElementById("textarea1").style.textAlign = "left";
-	document.getElementById("textarea1").style.fontStyle = "normal";
-	document.getElementById("textarea1").style.textTransform = "capitalize";
+	// document.getElementById("textarea1").style.fontWeight = "normal";
+	// document.getElementById("textarea1").style.textAlign = "left";
+	// document.getElementById("textarea1").style.fontStyle = "normal";
+	// document.getElementById("textarea1").style.textTransform = "capitalize";
 	document.getElementById("textarea1").value = " ";
 }
 
